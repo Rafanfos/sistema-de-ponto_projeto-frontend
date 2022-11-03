310bf23a384bd21bbf6e0e808899ed2a3fb876bc
Lucas <lucoder618@gmail.com>
2022-11-03 14:45:00 -0300

    feat/calendarComponent: add callback function support & add button references & add calendar mobile support

--- src/components/Calendar/style.ts
+++ src/components/Calendar/style.ts
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
+interface ICalendarProps {
+    buttonHeight: number
+}
+
 export const StyledCalendar = styled.div`
 
     .calendar-container{
         display: flex;
         flex-direction: column;
         align-items: center;
-        width: 350px;
     }
-    .calendar-icon{
+    .btn-open-calendar{
         padding: 5px 10px;
+        background-color: var(--grey-4);
+        border-radius: 4px;
+        border: 1px solid transparent;
+    }
+    .active{
+        border: 1px solid var(--grey-1);
     }
     .react-calendar{
         border-radius: 5px;
         overflow: hidden;
         position: absolute;
-        transform: translateY(38px);
+        transform: translateY(${(props:ICalendarProps) => props.buttonHeight + 2}px);
     }
     .react-calendar__navigation{
         background-color: var(--color-primary);
@@ -54,5 +63,11 @@ export const StyledCalendar = styled.div`
     }
     .react-calendar__tile--active{
         background-color: var(--color-secondary);
+    }
+    @media screen and (max-width: 330px){
+        .react-calendar{
+        width: 90vw;
+        margin: 0 auto;
+    }
     }
 `
